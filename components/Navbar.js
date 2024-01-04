8e25fd692a9340db25c99d86039d2f6c37f31ad5
SriCoD <72248660+SriCoD@users.noreply.github.com>
2024-01-04 22:07:25 +0530

    NAvbar changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'next/Link'
 const Navbar = () => {
   return (
-    <div>
+    <div >
 
-<header className="text-gray-600 body-font">
+<header className="text-gray-600 body-font bg-white">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
@@ -12,12 +12,12 @@ const Navbar = () => {
       </svg>
       <span className="ml-3 text-xl">Tailblocks</span>
     </a>
-    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-      <Link href="/"><a className="mr-5 hover:text-gray-900">Home   </a>       </Link>
-      <Link href="/about"><a className="mr-5 hover:text-gray-900">About    </a>       </Link>
-      <Link href="products"><a className="mr-5 hover:text-gray-900">Products  </a>         </Link>
-      <Link href="contact"><a className="mr-5 hover:text-gray-900">Contact Us</a>        </Link>
-    </nav>
+    <div className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+      <Link href="/" className="mr-5 hover:text-gray-900">Home         </Link>
+      <Link href="/about" className="mr-5 hover:text-gray-900">About          </Link>
+      <Link href="/products" className="mr-5 hover:text-gray-900">Products          </Link>
+      <Link href="/contact" className="mr-5 hover:text-gray-900">Contact Us       </Link>
+    </div>
     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -29,4 +29,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
